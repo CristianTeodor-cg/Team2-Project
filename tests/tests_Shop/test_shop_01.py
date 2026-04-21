e8a98fc166a88102502e4ed8cc06c2b22acc9dec
Akiyama <rika.akiyama@sogeti.com>
2026-04-21 17:06:57 +0200

    added test case show article in shop

---
+++ tests/tests_Shop/test_shop_01.py
@@ -0,0 +1,45 @@
+from playwright.sync_api import expect
+
+
+
+def test_shop_show_article(page):
+
+
+    #Navigate to page
+    page.goto("http://10.40.226.200/BC_Team_2/shop.php")
+
+    #Check if 'Column Header' are visible
+    expect(page.get_by_role("columnheader", name="Bild")).to_be_visible()
+    expect(page.get_by_role("columnheader", name="Hinzufügen")).to_be_visible()
+    expect(page.get_by_role("columnheader", name="Preis")).to_be_visible()
+    expect(page.get_by_role("columnheader", name="Name")).to_be_visible()
+
+    
+    rows = page.get_by_role("row").filter(has=page.locator("td"))
+    expect(rows).not_to_have_count(0)
+
+    count = rows.count()
+
+    for i in range(count):
+        row = rows.nth(i)
+
+        name = row.locator("td").nth(0)
+        price = row.locator("td").nth(1)
+        image = row.locator("td img")
+        add_button = row.get_by_role("link", name="Einkaufen")
+
+        expect(name).to_be_visible()
+        expect(name).not_to_have_text("")
+
+        expect(price).to_be_visible()
+        expect(price).to_contain_text("€")
+
+        expect(image).to_be_visible()
+        expect(image).to_have_attribute("src")
+
+        expect(add_button).to_be_visible()
+        expect(add_button).to_be_enabled()
+
+
+
+   
