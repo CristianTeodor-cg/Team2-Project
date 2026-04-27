7895f5466756a672026d0c4b5e08bacac233d30a
CrisianTeodor-cg <cristian.a.ilie@sogeti.com>
2026-04-27 15:24:52 +0200

    test_rabatt

--- tests/test_rabatt/test_rabatt_01.py
+++ tests/test_rabatt/test_rabatt_01.py
@@ -2,8 +2,15 @@ from page_objects import AppPage
 
 from playwright.sync_api import expect
 
+import re
+
+
 def test_Rabattfunktion_im_Warenkorb(page):
 
+
+    
+    def euro_str_to_float(value: str) -> float:
+        return float(value.replace(".", "").replace(",", "."))
 
     #Start on any page like
     page.goto("http://10.40.226.200/BC_Team_2/index.php")
@@ -15,24 +22,17 @@ def test_Rabattfunktion_im_Warenkorb(page):
     
     page.locator("#accountbar > a").click()
     app.login("MarkusTE", "Mark0426TE") 
-    #expect(page.locator("#loginContainer")).to_be_visible() 
 
-
-# als initiales Setup: Löschen des Warenkorbes:
-    page.locator('//*[@id="shoppingcart"]').click()
-    page.get_by_role("button", name="Warenkorb löschen").click()
     
 
-# die folgenden Schritte inkl. Änderung der Item-Anzahl sollen erfolgen
-
-
-
-# Schritt 1 ergibt keinen Rabatt, da die Item-Zahl zu gering ist
-   
+    
    # in den Shop gehen, "Ratingen" auswählen
+    
+    page.goto("http://10.40.226.200/BC_Team_2/shop.php")
 
-    page.goto('//*shop.php"]').click()
-    page.get_by_role("link", name="Einkaufen").click()
+    page.locator('#search').fill("Ratingen") 
+    page.locator('//*[@id="shoplist"]/tbody/tr[12]/td[4]/a').click()
+    
 
 
 
@@ -46,7 +46,16 @@ def test_Rabattfunktion_im_Warenkorb(page):
 
 
     expect(page.locator("#rabatt")).to_contain_text("Rabatt: 0,00 €")
-    expect(page.locator("#form")).to_contain_text("Es fehlen noch 87,02 €, um 5€ Nachlass auf den gesamten Einkauf zu erhalten")
+
+    expect(page.locator("#form")).to_contain_text("Es fehlen noch")
+
+    text = page.locator("css=#rabatt").inner_text()
+    
+    match = re.search(r"([0-9]+,[0-9]{2})", text)
+    rabatt_str_0 = match.group(1)
+    rabatt_0 = euro_str_to_float(rabatt_str_0)
+    assert rabatt_0 == 0,00
+
 
 
 
@@ -60,39 +69,72 @@ def test_Rabattfunktion_im_Warenkorb(page):
 
       # in den Shop gehen, "Ratingen" auswählen
 
-    page.locator('//*shop.php"]').click()
-    page.get_by_role("link", name="Einkaufen").click()
+
+    page.locator('//*[@id="carttable"]/tbody/tr[2]/td[2]/input').fill("10")
+    page.locator('//*[@id="buttonRefresh"]').click()
+
+    text = page.locator("css=#rabatt").inner_text()
+    
+    match = re.search(r"([0-9]+,[0-9]{2})", text)
+    rabatt_str_1 = match.group(1)
 
 
+    #page.goto("http://10.40.226.200/BC_Team_2/shop.php")
+
+    # page.locator('#search').fill("Ratingen")
+    # page.locator("#shoplist > tbody > tr:nth-child(14) > td:nth-child(1) > a").click()
+
+    # expect(page.locator('//*[@id="dc-quantity"]')).to_be_visible()
+    # page.locator('//*[@id="dc-quantity"]').fill("11")
+
+    # page.locator('//*[@id="dc-cartbtn"]').click()
+
+    
 
     # zum Warenkorb gehen
   
     
-    page.locator('//*[@id="shoppingcart"]').click()
-    page.locator("input[name=\"quantity[15]\"]").click()
-    page.locator("input[name=\"quantity[15]\"]").fill("10")
-    page.get_by_role("button", name="Warenkorb aktualisieren").click()
-    expect(page.locator("#carttable")).to_contain_text("2,00 €")
-    expect(page.get_by_text("Rabatt: 2,00 €")).to_be_visible()
+    
+    # page.locator('//*[@id="shoppingcart"]').click()
+    # page.locator("input[name=\"quantity[15]\"]").click()
+    # page.locator("input[name=\"quantity[15]\"]").fill("10")
+    # page.get_by_role("button", name="Warenkorb aktualisieren").click()
+    # expect(page.locator("#carttable")).to_contain_text("2,00 €")
+    # expect(page.get_by_text("Rabatt: 2,00 €")).to_be_visible()
 
 
 
     # Schritt 3 zeigt Itemzahl > 20, der Rabatt geht auf 20% und die Gesamtsumme des Warenkorb > 100 EUR, so dass noch zusätzlich 5,00 EUR abgezogen werden
 
    
-    page.locator('//*[@id="shoppingcart"]').click()
-    page.locator("input[name=\"quantity[12]\"]").click()
-    page.locator("input[name=\"quantity[12]\"]").fill("21")
-    page.locator("input[name=\"quantity[12]\"]").press("Enter")
-    page.get_by_role("button", name="Warenkorb aktualisieren").click()
-    expect(page.locator("#carttable")).to_contain_text("Rabatt")
+    # page.locator('//*[@id="shoppingcart"]').click()
+    # page.locator("input[name=\"quantity[12]\"]").click()
+    # page.locator("input[name=\"quantity[12]\"]").fill("21")
+    # page.locator("input[name=\"quantity[12]\"]").press("Enter")
+    # page.get_by_role("button", name="Warenkorb aktualisieren").click()
+
     
-    expect(page.locator("#carttable")).to_contain_text("18,88 €")
+    page.locator('//*[@id="carttable"]/tbody/tr[2]/td[2]/input').type("20")
+    page.locator('//*[@id="buttonRefresh"]').click()
+
+    
+
+    text = page.locator("css=#rabatt").inner_text()  
+    match = re.search(r"([0-9]+,[0-9]{2})", text)
+    rabatt_str_2 = match.group(1)  
+
+    
+    rabatt_1 = euro_str_to_float(rabatt_str_1)
+    rabatt_2 = euro_str_to_float(rabatt_str_2)
+
+
+    assert rabatt_2 > rabatt_1 > rabatt_0
+   
     
     
     
     # Gesamtsumme = Warenwert abzgl. Rabatt inkl. 5,00 EUR Reduktion, wenn Warenwert > 100 EUR
     
-    page.get_by_text("Warenwert: 228,69 €").click()
-    expect(page.locator("#rabatt")).to_contain_text("Rabatt: 25,87 €")
-    expect(page.locator("#gsum")).to_contain_text("Gesamtsumme: 202,82 €")
+    # page.get_by_text("Warenwert: 228,69 €").click()
+    # expect(page.locator("#rabatt")).to_contain_text("Rabatt: 25,87 €")
+    # expect(page.locator("#gsum")).to_contain_text("Gesamtsumme: 202,82 €")
