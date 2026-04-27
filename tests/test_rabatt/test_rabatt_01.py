47048c9b8f3e095cc48ad9119c63c0a6136e909c
MaReSogeti <markus.rehle@sogeti.com>
2026-04-27 14:10:11 +0200

    push

--- tests/test_rabatt/test_rabatt_01.py
+++ tests/test_rabatt/test_rabatt_01.py
@@ -18,22 +18,57 @@ def test_Rabattfunktion_im_Warenkorb(page):
     #expect(page.locator("#loginContainer")).to_be_visible() 
 
 
+# als initiales Setup: Löschen des Warenkorbes:
+    page.locator('//*[@id="shoppingcart"]').click()
+    page.get_by_role("button", name="Warenkorb löschen").click()
+    
+
 # die folgenden Schritte inkl. Änderung der Item-Anzahl sollen erfolgen
+
+
 
 # Schritt 1 ergibt keinen Rabatt, da die Item-Zahl zu gering ist
    
+   # in den Shop gehen, "Ratingen" auswählen
 
+    page.goto('//*shop.php"]').click()
+    page.get_by_role("link", name="Einkaufen").click()
+
+
+
+# zum Warenkorb gehen
   
-    #page.goto("http://10.40.226.200/BC_Team_2/shoppingcart.php")
+    
     page.locator('//*[@id="shoppingcart"]').click()
     page.locator("input[name=\"quantity[12]\"]").click()
+
+    # zeigt Itemzahl < 10, demnach kein Rabatt ausgewiesen
+
+
     expect(page.locator("#rabatt")).to_contain_text("Rabatt: 0,00 €")
     expect(page.locator("#form")).to_contain_text("Es fehlen noch 87,02 €, um 5€ Nachlass auf den gesamten Einkauf zu erhalten")
 
 
 
+
+
+
+
+
+
     # Schritt 2 zeigt eine Itemzahl > 10, demnach ist der Rabatt bei 10%
 
+      # in den Shop gehen, "Ratingen" auswählen
+
+    page.locator('//*shop.php"]').click()
+    page.get_by_role("link", name="Einkaufen").click()
+
+
+
+    # zum Warenkorb gehen
+  
+    
+    page.locator('//*[@id="shoppingcart"]').click()
     page.locator("input[name=\"quantity[15]\"]").click()
     page.locator("input[name=\"quantity[15]\"]").fill("10")
     page.get_by_role("button", name="Warenkorb aktualisieren").click()
@@ -44,7 +79,7 @@ def test_Rabattfunktion_im_Warenkorb(page):
 
     # Schritt 3 zeigt Itemzahl > 20, der Rabatt geht auf 20% und die Gesamtsumme des Warenkorb > 100 EUR, so dass noch zusätzlich 5,00 EUR abgezogen werden
 
-    #page.goto("http://10.40.226.200/BC_Team_2/shoppingcart.php")
+   
     page.locator('//*[@id="shoppingcart"]').click()
     page.locator("input[name=\"quantity[12]\"]").click()
     page.locator("input[name=\"quantity[12]\"]").fill("21")
